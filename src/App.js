550c6644a399a3cbfaefdb7fdbd0b6642a3b7a70
Tony <rodriguezanthony981@gmail.com>
2022-10-14 17:09:54 -0500

    envio

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
         <h4>APP Buscador de GIFS con API</h4>
         <Link to="/gif/Goku">Gifs de Goku</Link>
         <Link to="/gif/Vegeta">Gifs de Vegeta</Link>
-        <Link to="/gif/Gohan">Gifs de Gohan</Link>  
+        <Link to="/gif/Gohan">Gifs de Gohan</Link>  ss
         <Route 
           component={ListOfGifs} 
           path="/gif/:keyword"  />
