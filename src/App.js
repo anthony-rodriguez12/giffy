ebbc2465c6fa40c3577ff5ccf3bf6e4f17fa400a
Tony <rodriguezanthony981@gmail.com>
2022-10-18 12:36:52 -0500

    empezaremos hoy

--- src/App.js
+++ src/App.js
@@ -3,36 +3,42 @@ import "./App.css";
 import Home from './Pages/Home'
 import SearchResults from './Pages/SearchResults'
 import Detail from './Pages/Detail'
+import StaticContext from './Context/StaticContext'
+import {GifsContextProvider} from './Context/GifsContext'
+
 
 import ListOfGifs from './Components/ListOfGifs'
 
 import { Link, Route } from "wouter";
 
 export default function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <h4>APP Buscador de GIFS con API</h4>
-          <img src="https://imgs.search.brave.com/GOegr92r_v6i0kersM3n20eOh2VAMtTXqMcWiNLhBWA/rs:fit:320:320:1/g:ce/aHR0cDovL3BhMS5u/YXJ2aWkuY29tLzY4/MTIvMTkxMDFjYjc0/M2FmMDk5MTQ3MTll/ZjE2NTVkOTI2NGNk/ODdlNGVmN18wMC5n/aWY.gif" alt="Goku Feliz" />
-        </Link>
-        <Route 
-          component={Home}
-          path="/"
-        />
-        <Route 
-          component={SearchResults}
-          path="/search/:keyword"
-        />
-        <Route 
-          component={Detail}
-          path="/gif/:id"
-        />
-        <Route 
-          component={ListOfGifs}
-          path="/gif/:keyword"  />
-      </header>
-    </div>
+    <StaticContext.Provider value={ { name: 'miduDev', suscribeteAlCanal: true  } }>
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <h4>APP Buscador de GIFS con API</h4>
+            <img src="https://imgs.search.brave.com/GOegr92r_v6i0kersM3n20eOh2VAMtTXqMcWiNLhBWA/rs:fit:320:320:1/g:ce/aHR0cDovL3BhMS5u/YXJ2aWkuY29tLzY4/MTIvMTkxMDFjYjc0/M2FmMDk5MTQ3MTll/ZjE2NTVkOTI2NGNk/ODdlNGVmN18wMC5n/aWY.gif" alt="Goku Feliz" />
+          </Link>
+          <GifsContextProvider.Provider value={}>
+            <Route 
+              component={Home}
+              path="/"
+            />
+            <Route 
+              component={SearchResults}
+              path="/search/:keyword"
+            />
+            <Route 
+              component={Detail}
+              path="/gif/:id"
+            />
+            <Route 
+              component={ListOfGifs}
+              path="/gif/:keyword"  />
+          </GifsContextProvider.Provider>
+        </section>
+      </div>
+    </StaticContext.Provider>
   );
 }
--- src/Pages/Detail/index.jsx
+++ src/Pages/Detail/index.jsx
@@ -1,4 +1,13 @@
+import { useContext } from 'react'
+import StaticContext from '../../Context/StaticContext'
+import GifsContext from '../../Context/GifsContext'
+
 export default function Detail({ params }) {
-   console.log(params.id)
+   const staticConfig = useContext(StaticContext)
+   console.log(staticConfig)
+
+   const gifsContext = useContext(GifsContext)
+   console.log(gifsContext)
+   //   console.log(params.id)
    return <h1>Gif con id {params.id}</h1>
 }
